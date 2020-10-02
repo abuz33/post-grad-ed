4d6059eb3db398f4a462bca94b49d165460296ee
Abuzer ALACA <aalaca.33@gmail.com>
2020-10-03 00:14:41 +0200

    Completed Project

--- TA/TAWeek1And2/Assignment-DatHuis/src/entry.js
+++
@@ -1,7 +0,0 @@
-import 'react-hot-loader'
-
-import ReactDOM from 'react-dom'
-import React from 'react'
-import HotApp from './HotApp'
-
-ReactDOM.render(<HotApp />, document.querySelector('#app'))
